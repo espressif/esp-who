456cdafea28ca6b416e3f10e45b321ef121c7619
Yuan Jiong <yuanjiong@espressif.com>
2019-07-08 13:36:34 +0800

    add example with sdcard

--- examples/single_chip/test_system/main/app_main.c
+++ examples/single_chip/test_system/main/app_main.c
@@ -27,6 +27,6 @@
 void app_main()
 {
     app_wifi_main();
-    app_camera_main();
+    //app_camera_main();
     app_httpd_main();
 }
--- examples/single_chip/test_system/main/app_wifi.c
+++ examples/single_chip/test_system/main/app_wifi.c
@@ -110,6 +110,7 @@ void wifi_init_softap()
     if (strlen(EXAMPLE_ESP_WIFI_AP_PASS) == 0) {
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
+    esp_wifi_set_ps(WIFI_PS_NONE);
 
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
 
