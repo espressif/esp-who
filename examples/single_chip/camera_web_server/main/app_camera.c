c165d6367e547eb55eff5affbd4e5412f052d150
Bond Keevil <me@bondkeevil.ca>
2019-07-28 15:23:07 -0400

    Feature: Control a LED Illuminator or Flash from Camera_Web_Server
    Add a "LED Illuminator" menu to menuconfig
    Through menuconfig you can select a LEDC pin, LEDC Timer, LEDC Channel, and LEDC Speed Mode
    The defaults work for the AIThinker ESPCAM-32 board
    There is an option to limit LED intensity while streaming to prevent overheating
    LED intensity is not limited when taking a still image photo
    The LED intensity controls are hidden on the web page if the LED feature is not enabled
    LED intensity can be adjusted while streaming and the LED will respond right away
    
    Tested on an ESPCAM-32 board with an OV2460 sensor

--- examples/single_chip/camera_web_server/main/app_camera.c
+++ examples/single_chip/camera_web_server/main/app_camera.c
@@ -20,6 +20,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 #include "esp_log.h"
+#include "driver/ledc.h"
 #include "esp_camera.h"
 #include "app_camera.h"
 #include "sdkconfig.h"
@@ -42,6 +43,36 @@ void app_camera_main ()
     gpio_config(&conf);
     conf.pin_bit_mask = 1LL << 14;
     gpio_config(&conf);
+#endif
+
+#ifdef CONFIG_LED_ILLUMINATOR_ENABLED
+    gpio_set_direction(CONFIG_LED_LEDC_PIN,GPIO_MODE_OUTPUT);
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_8_BIT,            // resolution of PWM duty
+        .freq_hz         = 1000,                        // frequency of PWM signal
+        .speed_mode      = LEDC_LOW_SPEED_MODE,  // timer mode
+        .timer_num       = CONFIG_LED_LEDC_TIMER        // timer index
+    };
+    ledc_channel_config_t ledc_channel = {
+        .channel    = CONFIG_LED_LEDC_CHANNEL,
+        .duty       = 0,
+        .gpio_num   = CONFIG_LED_LEDC_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .hpoint     = 0,
+        .timer_sel  = CONFIG_LED_LEDC_TIMER
+    };
+    #ifdef CONFIG_LED_LEDC_HIGH_SPEED_MODE
+    ledc_timer.speed_mode = ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    #endif
+    switch (ledc_timer_config(&ledc_timer)) {
+        case ESP_ERR_INVALID_ARG: ESP_LOGE(TAG, "ledc_timer_config() parameter error"); break;
+        case ESP_FAIL: ESP_LOGE(TAG, "ledc_timer_config() Can not find a proper pre-divider number base on the given frequency and the current duty_resolution"); break;
+        case ESP_OK: if (ledc_channel_config(&ledc_channel) == ESP_ERR_INVALID_ARG) {
+            ESP_LOGE(TAG, "ledc_channel_config() parameter error");
+          }
+          break;
+        default: break;
+    }
 #endif
 
     camera_config_t config;
