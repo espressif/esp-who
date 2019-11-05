27e8cde72c1d6b308f404bd7f8535bb1b20c9695
XiaochaoGONG <gongxiaochao@espressif.com>
2019-11-05 20:00:09 +0800

    [bugfix] add lssh in make

--- components/component_conf.mk
+++ components/component_conf.mk
@@ -3,3 +3,5 @@ EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-face/lib
 EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-face/image_util
 EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-face/face_detection
 EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-face/face_recognition
+EXTRA_COMPONENT_DIRS += $(SOLUTION_PATH)/components/esp-face/object_detection
+
