811ebfd00ae2ac9e818be88d0304166e3536544b
Ye Hang Yang <yehangyang@espressif.com>
2021-09-10 10:53:36 +0800

    :sparkles: human_face_detection display on lcd

---
+++ examples/human_face_detection/lcd/main/app_main.cpp
@@ -0,0 +1,16 @@
+#include "who_camera.h"
+#include "who_human_face_detection.hpp"
+#include "who_lcd.h"
+
+static QueueHandle_t xQueueAIFrame = NULL;
+static QueueHandle_t xQueueLCDFrame = NULL;
+
+extern "C" void app_main()
+{
+    xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+    xQueueLCDFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+
+    register_camera(PIXFORMAT_RGB565, FRAMESIZE_240X240, 2, xQueueAIFrame);
+    register_human_face_detection(xQueueAIFrame, NULL, NULL, xQueueLCDFrame, false);
+    register_lcd(xQueueLCDFrame, NULL, true);
+}
