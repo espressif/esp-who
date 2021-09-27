4cc21d496be5946cb159fb918a7617c117907260
Ye Hang Yang <yehangyang@espressif.com>
2021-09-27 15:24:28 +0800

    :sparkles: cat_face_detection module

--- components/modules/ai/who_cat_face_detection.hpp
+++ components/modules/ai/who_cat_face_detection.hpp
@@ -5,5 +5,8 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 
-void register_cat_face_detection(QueueHandle_t frame_i, QueueHandle_t event,
-                               QueueHandle_t result, QueueHandle_t frame_o = NULL);
+void register_cat_face_detection(QueueHandle_t frame_i,
+                                 QueueHandle_t event,
+                                 QueueHandle_t result,
+                                 QueueHandle_t frame_o,
+                                 const bool camera_fb_return);
