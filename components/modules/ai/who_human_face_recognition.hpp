1b785f3bd1394e402dc1eb111d4814eb657251e9
yuanjiong <yuanjiong@espressif.com>
2021-10-26 17:02:41 +0800

    :art: format code

--- components/modules/ai/who_human_face_recognition.hpp
+++ components/modules/ai/who_human_face_recognition.hpp
@@ -15,7 +15,7 @@ typedef enum
 } recognizer_state_t;
 
 void register_human_face_recognition(QueueHandle_t frame_i,
-                                   QueueHandle_t event,
-                                   QueueHandle_t result,
-                                   QueueHandle_t frame_o = NULL,
-                                   const bool camera_fb_return = false);
+                                     QueueHandle_t event,
+                                     QueueHandle_t result,
+                                     QueueHandle_t frame_o = NULL,
+                                     const bool camera_fb_return = false);
