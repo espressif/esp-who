e5d6cd4c8356bbcbd87c9bb25435f4f29ed4c5ce
yuanjiong <yuanjiong@espressif.com>
2021-11-26 20:03:10 +0800

    :art: update modules

--- components/modules/lcd/who_lcd.h
+++ components/modules/lcd/who_lcd.h
@@ -4,13 +4,21 @@
 #include "esp_log.h"
 #include "screen_driver.h"
 
-#define BOARD_LCD_MOSI 46
+#define BOARD_LCD_MOSI 47
 #define BOARD_LCD_MISO -1
 #define BOARD_LCD_SCK 21
 #define BOARD_LCD_CS 44
-#define BOARD_LCD_DC 47
+#define BOARD_LCD_DC 43
 #define BOARD_LCD_RST -1
 #define BOARD_LCD_BL 48
+#define BOARD_LCD_PIXEL_CLOCK_HZ (40 * 1000 * 1000)
+#define BOARD_LCD_BK_LIGHT_ON_LEVEL 0
+#define BOARD_LCD_BK_LIGHT_OFF_LEVEL !BOARD_LCD_BK_LIGHT_ON_LEVEL
+#define BOARD_LCD_H_RES 240
+#define BOARD_LCD_V_RES 240
+#define BOARD_LCD_CMD_BITS 8
+#define BOARD_LCD_PARAM_BITS 8
+#define LCD_HOST SPI2_HOST
 
 #ifdef __cplusplus
 extern "C"
