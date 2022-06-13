7f7b0ae9a79a6498642d64d7957d28ba23c0c660
valeriojr <41243914+valeriojr@users.noreply.github.com>
2022-06-13 13:37:00 -0300

    ✏typo in comment
    blightness -> brightness

--- components/modules/camera/who_camera.c
+++ components/modules/camera/who_camera.c
@@ -78,7 +78,7 @@ void register_camera(const pixformat_t pixel_fromat,
     //initial sensors are flipped vertically and colors are a bit saturated
     if (s->id.PID == OV3660_PID)
     {
-        s->set_brightness(s, 1);  //up the blightness just a bit
+        s->set_brightness(s, 1);  //up the brightness just a bit
         s->set_saturation(s, -2); //lower the saturation
     }
 
