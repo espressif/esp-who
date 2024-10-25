1a8dc30e3b32f089016453d3768dc97a94f46aad
Fan Shen Wei <fanshenwei@espressif.com>
2024-10-25 14:19:00 +0800

    clean old version files.

--- components/modules/ai/who_motion_detection.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/queue.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-
-void register_motion_detection(QueueHandle_t frame_i, QueueHandle_t event,
-                               QueueHandle_t result, QueueHandle_t frame_o = NULL);
